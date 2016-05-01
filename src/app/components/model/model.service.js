4b88005383abad901153733ba347765e7d222236
Jon Welker <JWelker110@gmail.com>
2016-05-01 14:26:47 -0500

    Navbar is its own component meow

--- src/app/components/model/model.service.js
+++ src/app/components/model/model.service.js
@@ -23,12 +23,53 @@
           name: 'Browse'
         }
       ],
+
       username: null,
       confirmed: false,
 
       offset: 0,
       category: null,
+
       currentItem: null,
+      currentItemMeta: null,
+      items: [
+        {
+          'id': 1,
+          'name': 'first item',
+          'description': 'this is the first item',
+          'owner_name': 'JohnDoe25'
+        },
+        {
+          'id': 2,
+          'name': 'second item',
+          'description': 'this is the first item',
+          'owner_name': 'JohnDoe25'
+        },
+        {
+          'id': 1,
+          'name': 'first item',
+          'description': 'this is the first item',
+          'owner_name': 'JohnDoe25'
+        },
+        {
+          'id': 2,
+          'name': 'second item',
+          'description': 'this is the first item',
+          'owner_name': 'JohnDoe25'
+        },
+        {
+          'id': 1,
+          'name': 'first item',
+          'description': 'this is the first item',
+          'owner_name': 'JohnDoe25'
+        },
+        {
+          'id': 2,
+          'name': 'second item',
+          'description': 'this is the first item',
+          'owner_name': 'JohnDoe25'
+        }
+      ],
 
       stayLoggedIn: true,
 
