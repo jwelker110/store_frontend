8cd548059f429f2092577c3c87e45bf74c8fc566
Jon Welker <JWelker110@gmail.com>
2016-05-02 22:29:50 -0500

    Item details are now retrieved and shown on the item template

--- src/app/items/item.controller.js
+++ src/app/items/item.controller.js
@@ -5,46 +5,17 @@
       .module('frontend')
       .controller('ItemController', ItemController);
 
-  ItemController.$inject = ['$stateParams'];
+  ItemController.$inject = ['$stateParams', 'Model'];
 
-  function ItemController($stateParams){
+  function ItemController($stateParams, Model){
     var vm = this;
+
+    vm.Model = Model;
 
     vm.itemId = $stateParams.itemId;
     vm.itemName = $stateParams.itemName;
 
-    // make a call to the items service to retrieve the item
-    vm.item = {
-      'id': 1,
-      'name': 'first item',
-      'description': 'this is the first item',
-      'owner_name': 'JohnDoe25'
-    };
-
-    vm.itemMeta = [
-      {
-        "id": 1,
-        "item_id": vm.item.id,
-        "image_url": null,
-        "price": 25.50,
-        "sale_price": 15.99,
-        "stock": 5,
-        "description": "This is the purple item's first meta description",
-        "meta_key": "Color",
-        "meta_value": "Purple"
-      },
-      {
-        "id": 1,
-        "item_id": vm.item.id,
-        "image_url": null,
-        "price": 25.50,
-        "sale_price": 15.99,
-        "stock": 5,
-        "description": "This is the green item's first meta description",
-        "meta_key": "Color",
-        "meta_value": "Green"
-      }
-    ]
+    vm.Model.setCurrentItem(vm.itemName);
 
   }
 
