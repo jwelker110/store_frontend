ffbaa616b5189e724aea6c5da485f68a4f3f6def
Jon Welker <JWelker110@gmail.com>
2016-05-11 20:45:56 -0500

    Add user info to settings page

--- src/app/user/settings.controller.js
+++ src/app/user/settings.controller.js
@@ -5,10 +5,18 @@
       .module('frontend')
       .controller('SettingsController', SettingsController);
 
-  SettingsController.$inject = [];
+  SettingsController.$inject = ['Model'];
 
-  function SettingsController(){
+  function SettingsController(Model){
     var vm = this;
+
+    vm.Model = Model;
+
+    vm.updateSettings = updateSettings;
+
+    function updateSettings(){
+      // update user settings here
+    }
 
   }
 })();
