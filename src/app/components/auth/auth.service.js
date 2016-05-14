fad203a2a36e4aac2bc9c04846207fec5bb65328
Jon Welker <JWelker110@gmail.com>
2016-05-14 15:14:06 -0500

    Remove unused ep's associated with user accounts

--- src/app/components/auth/auth.service.js
+++ src/app/components/auth/auth.service.js
@@ -9,42 +9,6 @@
   function Auth($resource){
 
     return {
-      register: $resource('http://localhost:8080/register', {},
-          {
-            submit: {
-              method: 'POST',
-              params: {
-                first_name: null,
-                last_name: null,
-                email: null,
-                username: null,
-                password: null,
-                g_recaptcha_response: null
-              }
-            }
-          }),
-      login: $resource('http://localhost:8080/login', {},
-          {
-            submit: {
-              method: 'POST',
-              params: {
-                username: null,
-                password: null
-              }
-            }
-          }),
-      confirm: $resource('http://localhost:8080/confirm', {},
-          {
-            submit: {
-              method: 'POST',
-              params: {
-                token: null,
-                jwt_token: null,
-                username: null,
-                confirmed: null
-              }
-            }
-          }),
       reauth: $resource('http://localhost:8080/reauth', {},
           {
             submit: {
@@ -71,20 +35,6 @@
                 email: null,
                 oa_id: null,
                 username: null
-              }
-            }
-          }),
-      settings: $resource('http://localhost:8080/settings', {},
-          {
-            update: {
-              method: 'POST',
-              params: {
-                username: null,
-                email: null,
-                oa_id: null,
-                password: null,
-                old_password: null,
-                avatar_url: null
               }
             }
           })
