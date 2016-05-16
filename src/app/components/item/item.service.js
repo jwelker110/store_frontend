66bdba11a859c871e09fbd7cc284c3b1bc009dd8
Jon Welker <JWelker110@gmail.com>
2016-05-15 19:09:07 -0500

    Add create resource

--- src/app/components/item/item.service.js
+++ src/app/components/item/item.service.js
@@ -9,7 +9,21 @@
   function Item($resource){
 
     return {
-      items: $resource('http://localhost:8080/api/v1/items.json', {offset: 0, category: null}),
+      items: $resource('http://localhost:8080/api/v1/items.json', {offset: 0, category: null},
+          {
+            create: {
+              method: 'POST',
+              params: {
+                jwt_token: null,
+                name: null,
+                description: null,
+                price: null,
+                sale_price: null,
+                image_url: null,
+                stock: null
+              }
+            }
+          }),
       itemDetails: $resource('http://localhost:8080/api/v1/items/details.json', {name: null})
     };
 
