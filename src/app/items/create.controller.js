d1b05eddfdf3affab6b80a27c253f7c7899255a2
Jon Welker <JWelker110@gmail.com>
2016-05-15 22:35:54 -0500

    Add ability to create item records

---
+++ src/app/items/create.controller.js
@@ -0,0 +1,39 @@
+(function(){
+  "use strict";
+
+  angular.module('frontend')
+      .controller('CreateController', CreateController);
+
+  CreateController.$inject = ['Item', 'Model'];
+
+  function CreateController(Item, Model){
+    var vm = this;
+
+    vm.Model = Model;
+
+    vm.name = null;
+    vm.description = null;
+    vm.price = 0.00;
+    vm.sale_price = 0.00;
+    vm.stock = 0;
+
+    vm.createItem = createItem;
+
+    function createItem() {
+      var newItem = Item.items.create({
+        jwt_token: Model.getJwtString(),
+        name: vm.name,
+        description: vm.description,
+        price: vm.price,
+        sale_price: vm.sale_price,
+        stock: vm.stock
+      });
+
+      newItem.$promise.then(function(data){
+        // new item was created, let's update the UI / User
+      });
+    }
+
+  }
+
+})();
