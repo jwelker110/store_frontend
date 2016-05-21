93926daafafef6d587365bc833e5ff0ca9463325
Jon Welker <JWelker110@gmail.com>
2016-05-21 10:11:34 -0500

    Add remove method to itemDetails ep

--- src/app/components/item/item.service.js
+++ src/app/components/item/item.service.js
@@ -24,7 +24,28 @@
               }
             }
           }),
-      itemDetails: $resource('http://localhost:8080/api/v1/items/details.json', {name: null})
+      itemDetails: $resource('http://localhost:8080/api/v1/items/details.json', {name: null},
+          {
+            update: {
+              method: 'PUT',
+              params: {
+                jwt_token: null,
+                name: null,
+                description: null,
+                price: null,
+                sale_price: null,
+                image_url: null,
+                stock: null
+              }
+            },
+            remove: {
+              method: 'POST',
+              params: {
+                jwt_token: null,
+                itemName: null
+              }
+            }
+          })
     };
 
   }
