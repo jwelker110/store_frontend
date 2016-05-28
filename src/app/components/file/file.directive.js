34cdc4cc61c2ad2648a6afc067e6172e9b1dcf23
Jon Welker <JWelker110@gmail.com>
2016-05-28 17:53:18 -0500

    Comment added

--- src/app/components/file/file.directive.js
+++ src/app/components/file/file.directive.js
@@ -7,12 +7,16 @@
   FileInput.$inject = [];
 
   function FileInput(){
+    /**
+     * This directive assigns the file to the controller on
+     * the var it is passed
+     */
     return {
       restrict: 'A',
       scope: {
         itemFile: '=fileInput'
       },
-      link: function(scope, elm, attrs){
+      link: function(scope, elm){
         elm.bind('change', function(){
           scope.$apply(function(){
             scope.itemFile = elm[0].files[0];
